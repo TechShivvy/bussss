65c6e02ea5147ff7f3412b10df2f71dcf4147f8a
Shivcharan Thirunavukkarasu <132028666+TechShivvy@users.noreply.github.com>
2023-10-09 17:43:43 +0530

    6969

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 // const SOCKET_URL = process.env.SERVER_URL || "http://localhost:8080";
-const SOCKET_URL = "https://bus-oucf.onrender.com:6969";
+const SOCKET_URL = "https://bus-oucf.onrender.com";
 const socket = io(SOCKET_URL);
 export default socket;
